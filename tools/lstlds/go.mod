bb73a4dca9dbd4c5ff5a6007a28253a3bcdd65cd
Jake Ireland <jakewilliami@icloud.com>
2023-11-04 07:53:12 +1300

    Implement helper tool to persist TLD data
    Refs:
      https://www.wikiwand.com/en/Top-level_domain
      https://www.wikiwand.com/en/List_of_Internet_top-level_domains
      https://www.wikiwand.com/en/Country_code_top-level_domain
      https://www.wikiwand.com/en/Generic_top-level_domain
      https://www.wikiwand.com/en/Internationalized_country_code_top-level_domain
      https://www.iana.org/domains/root/db

---
+++ tools/lstlds/go.mod
@@ -0,0 +1,7 @@
+module github.com/jakewilliami/tldeets/scripts/lstlds
+
+go 1.21.3
+
+require github.com/nfx/go-htmltable v0.4.0
+
+require golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
---
+++ tools/lstlds/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	// https://stackoverflow.com/a/74328802
+	"github.com/nfx/go-htmltable"
+)
+
+// TLD types
+// https://www.sohamkamani.com/golang/enums/
+const (
+	Generic     string = "generic"
+	CountryCode        = "country-code"
+)
+
+type TLD struct {
+	Domain  string `header:"Domain"`
+	Type    string `header:"Type"`
+	Manager string `header:"TLD Manager"`
+}
+
+func main() {
+	htmltable.Logger = func(_ context.Context, msg string, fields ...any) {
+		fmt.Printf("[INFO] %s %v\n", msg, fields)
+	}
+
+	url := "https://www.iana.org/domains/root/db"
+	table, err := htmltable.NewSliceFromURL[TLD](url)
+	if err != nil {
+		fmt.Printf("[ERROR] Could not get table by %s: %s", url, err)
+		os.Exit(1)
+	}
+
+	data := make([]TLD, len(table))
+	for i := 0; i < len(table); i++ {
+		data[i] = table[i]
+	}
+
+	tldJson, err := json.Marshal(data)
+	if err != nil {
+		fmt.Printf("[ERROR] Could not JSONify data: %s\n", err)
+		os.Exit(1)
+	}
+
+	outFile := "../../assets/tlds.json"
+	err = ioutil.WriteFile(outFile, tldJson, 0644)
+	if err != nil {
+		fmt.Printf("[ERROR] Count not write JSON output to %s: %s", outFile, err)
+		os.Exit(1)
+	}
+
+	// fmt.Printf("[DEBUG] %+v\n", data)
+	fmt.Printf("[INFO] Wrote %d bytes to \"%s\"\n", len(tldJson), outFile)
+}
