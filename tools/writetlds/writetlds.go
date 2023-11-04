98331cc9a7897679fa9ecca2e1f212358223ca75
Jake Ireland <jakewilliami@icloud.com>
2023-11-05 10:08:37 +1300

    Persist TLDs to pkg file!  v0.0.3
    Rather than writing the TLD data to a JSON file, we write it to a Go
    file that exists as part of the package, so that the data are easy to
    use for the user of the library.

--- tools/writetlds/writetlds.go
+++ tools/writetlds/writetlds.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"runtime"
+	"slices"
+	"strconv"
 	"strings"
 
-	// https://stackoverflow.com/a/74328802
 	"github.com/biter777/countries"
 	"github.com/nfx/go-htmltable"
 
@@ -30,6 +33,22 @@ type TLD struct {
 	Manager string          `header:"TLD Manager"`
 }
 
+func checkWriterErr(err error, file string) {
+	if err != nil {
+		fmt.Printf("[ERROR] Could not write line to file \"%s\": %s", file, err)
+		os.Exit(1)
+	}
+}
+
+func isASCII(s string) bool {
+	for _, char := range s {
+		if char > 127 {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	fmt.Printf("[INFO] Found base module path at %s\n", rootpath)
 
@@ -37,6 +56,7 @@ func main() {
 		fmt.Printf("[INFO] %s %v\n", msg, fields)
 	}
 
+	// https://stackoverflow.com/a/74328802
 	url := "https://www.iana.org/domains/root/db"
 	table, err := htmltable.NewSliceFromURL[TLD](url)
 	if err != nil {
@@ -56,6 +76,7 @@ func main() {
 		// TODO: this will not always work; e.g. Saint Helena is has ccTLD .ac,
 		// but country code SH.  Another example: .su is for Soviet Union, but
 		// as it is no longer a country (e.g., ISO 3166-3).
+		// NOTE: while biter777/countries has domains, it's not complete
 		if tld.Type == tldeets.CountryCode {
 			var countryCode string
 			if tldStr[0] == '.' {
@@ -75,19 +96,98 @@ func main() {
 		}
 	}
 
-	tldJson, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
-		fmt.Printf("[ERROR] Could not JSONify data: %s\n", err)
+	writeMode := "const"
+	allowedWriteModes := []string{"const", "json"}
+	if len(os.Args) > 1 {
+		writeMode = os.Args[1]
+		if !slices.Contains(allowedWriteModes, writeMode) {
+			fmt.Printf("[ERROR] Invalid write mode \"%s\"; allowed modes: %v\n", writeMode, allowedWriteModes)
+			os.Exit(1)
+		}
+	} else {
+		fmt.Printf("[ERROR] Must specify a write mode to output; allowed modes: %v\n", allowedWriteModes)
 		os.Exit(1)
 	}
 
-	outFile := filepath.Join(rootpath, "assets", "tlds.json")
-	err = ioutil.WriteFile(outFile, tldJson, 0644)
-	if err != nil {
-		fmt.Printf("[ERROR] Count not write JSON output to %s: %s", outFile, err)
-		os.Exit(1)
-	}
+	if writeMode == "const" {
+		pkgName := "tldeets"
+		outFile := filepath.Join(rootpath, "pkg", pkgName, "tldsconst.go")
 
-	// fmt.Printf("[DEBUG] %+v\n", data)
-	fmt.Printf("[INFO] Wrote %d bytes to \"%s\"\n", len(tldJson), outFile)
+		file, err := os.Create(outFile)
+		if err != nil {
+			fmt.Printf("[ERROR] Cannot open file \"%s\": %s\n", outFile, err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		writer := bufio.NewWriter(file)
+
+		_, err = writer.WriteString("package " + pkgName + "\n\n")
+		checkWriterErr(err, outFile)
+		_, err = writer.WriteString("var (\n")
+		checkWriterErr(err, outFile)
+
+		tldsSkipped := 0
+		for _, tld := range data {
+			if !isASCII(tld.Domain) {
+				tldsSkipped++
+				continue
+			}
+
+			var tldPrefix string
+			if tld.Domain[0] == '.' {
+				tldPrefix = tld.Domain[1:]
+			}
+			if tld.Type == tldeets.CountryCode {
+				tldPrefix = strings.ToUpper(tldPrefix)
+			} else {
+				tldPrefix = strings.Title(tldPrefix)
+			}
+			// TODO: is this a good naming scheme for these constants?
+			_, err = writer.WriteString(fmt.Sprintf("%sTopLevelDomain = TLD{\nDomain: \"%s\",\nType: \"%s\",\nManager: %s,\nCountry: \"%s\",\n}\n", tldPrefix, tld.Domain, tld.Type, strconv.Quote(tld.Manager), tld.Country))
+			checkWriterErr(err, outFile)
+		}
+
+		fmt.Printf("[WARNING] Skipped %d non-unicode domain name(s)\n", tldsSkipped)
+
+		_, err = writer.WriteString(")\n")
+		checkWriterErr(err, outFile)
+
+		err = writer.Flush()
+		if err != nil {
+			fmt.Printf("[ERROR] Could not flush file writer: %s", err)
+			os.Exit(1)
+		}
+
+		fileInfo, err := os.Stat(outFile)
+		if err == nil {
+			fmt.Printf("[INFO] Wrote %d bytes to \"%s\"\n", fileInfo.Size(), outFile)
+		}
+
+		// TODO: Would like to do this without calling to external command
+		// Consider using: https://github.com/mvdan/gofumpt
+		cmd := exec.Command("go", "fmt", outFile)
+		err = cmd.Run()
+		if err != nil {
+			fmt.Printf("[WARNING] Failed to run `go fmt` on output file \"%s\": %s\n", outFile, err)
+		} else {
+			fmt.Printf("[INFO] Successfully ran `go fmt` on output file \"%s\"\n", outFile)
+		}
+	} else if writeMode == "json" {
+		tldJson, err := json.MarshalIndent(data, "", "  ")
+		if err != nil {
+			fmt.Printf("[ERROR] Could not JSONify data: %s\n", err)
+			os.Exit(1)
+		}
+
+		outFile := filepath.Join(rootpath, "assets", "tlds.json")
+		err = ioutil.WriteFile(outFile, tldJson, 0644)
+		if err != nil {
+			fmt.Printf("[ERROR] Count not write JSON output to %s: %s", outFile, err)
+			os.Exit(1)
+		}
+
+		// fmt.Printf("[DEBUG] %+v\n", data)
+		fmt.Printf("[INFO] Wrote %d bytes to \"%s\"\n", len(tldJson), outFile)
+	}
 }
